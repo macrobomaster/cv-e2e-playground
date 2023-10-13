e4ee62bcacd325c1d224ea2e5e53da759125acf1
Woze Parrot <wozeparrot@gmail.com>
2023-10-13 14:23:08 -0400

    feat: cleanup model a bit

--- model.py
+++ model.py
@@ -15,18 +15,34 @@ class ConvBlock:
         return x + residual
 
 
+class ConvEmbedding:
+    def __init__(self, c_in, out_dim, reduction="max"):
+        self.pre_conv = ConvBlock(c_in, out_dim // 2)
+        self.post_conv = Conv2d(out_dim // 2, out_dim, kernel_size=3)
+        assert reduction in ["max", "mean", "sum"], "reduction must be max, mean, or sum"
+        self.reduction = reduction
+
+    def __call__(self, x: Tensor):
+        x = self.pre_conv(x)
+        x = self.post_conv(x)
+        if self.reduction == "max":
+            x = x.max((2, 3))
+        elif self.reduction == "mean":
+            x = x.mean((2, 3))
+        elif self.reduction == "sum":
+            x = x.sum((2, 3))
+        return x
+
+
 class Head:
     def __init__(self):
-        self.c1 = ConvBlock(512, 256)
-        self.c_out = Conv2d(256, 512, kernel_size=3, bias=False)
-        self.color = Embedding(2, 512)
+        self.conv_emb = ConvEmbedding(512, 512)
+        self.color_emb = Embedding(2, 512)
         self.joint = Linear(512 * 2, 512)
         self.l_out = Linear(512, 4, bias=False)
 
     def __call__(self, x: Tensor, color: Tensor):
-        x = self.c1(x)
-        x = self.c_out(x)
-        x = x.max((2, 3))
-        color = self.color(color).reshape(x.shape[0], x.shape[1])
+        x = self.conv_emb(x)
+        color = self.color_emb(color).reshape(x.shape[0], x.shape[1])
         x = self.joint(x.cat(color, dim=1)).leakyrelu()
         return self.l_out(x)
