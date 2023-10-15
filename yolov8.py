93caa3309aef24adc4a110914d0dc5601ee392b1
Woze Parrot <wozeparrot@gmail.com>
2023-10-15 00:04:57 -0400

    feat: strip more stuff

--- yolov8.py
+++ yolov8.py
@@ -1,6 +1,4 @@
 from tinygrad.nn import Conv2d, BatchNorm2d
-from tinygrad.tensor import Tensor
-from itertools import chain
 from math import inf
 
 
@@ -22,30 +20,6 @@ def get_variant_multiples(variant):
         "l": (1.0, 1.0, 1.0),
         "x": (1, 1.25, 1.0),
     }.get(variant, None)
-
-
-class Upsample:
-    def __init__(self, scale_factor: int, mode: str = "nearest") -> None:
-        assert mode == "nearest"  # only mode supported for now
-        self.mode = mode
-        self.scale_factor = scale_factor
-
-    def __call__(self, x: Tensor) -> Tensor:
-        assert len(x.shape) > 2 and len(x.shape) <= 5
-        (b, c), _lens = x.shape[:2], len(x.shape[2:])
-        tmp = x.reshape([b, c, -1] + [1] * _lens) * Tensor.ones(
-            *[1, 1, 1] + [self.scale_factor] * _lens
-        )
-        return (
-            tmp.reshape(list(x.shape) + [self.scale_factor] * _lens)
-            .permute(
-                [0, 1]
-                + list(
-                    chain.from_iterable([[y + 2, y + 2 + _lens] for y in range(_lens)])
-                )
-            )
-            .reshape([b, c] + [x * self.scale_factor for x in x.shape[2:]])
-        )
 
 
 class Conv_Block:
@@ -150,9 +124,6 @@ class Darknet:
         ]
         self.b5 = [SPPF(int(512 * w * r), int(512 * w * r), 5)]
 
-    def return_modules(self):
-        return [*self.b1, *self.b2, *self.b3, *self.b4, *self.b5]
-
     def __call__(self, x):
         x1 = x.sequential(self.b1)
         x2 = x1.sequential(self.b2)
@@ -160,35 +131,3 @@ class Darknet:
         x4 = x3.sequential(self.b4)
         x5 = x4.sequential(self.b5)
         return (x2, x3, x5)
-
-
-class Yolov8NECK:
-    def __init__(self, w, r, d):  # width_multiple, ratio_multiple, depth_multiple
-        self.up = Upsample(2, mode="nearest")
-        self.n1 = C2f(
-            c1=int(512 * w * (1 + r)), c2=int(512 * w), n=round(3 * d), shortcut=False
-        )
-        self.n2 = C2f(c1=int(768 * w), c2=int(256 * w), n=round(3 * d), shortcut=False)
-        self.n3 = Conv_Block(
-            c1=int(256 * w), c2=int(256 * w), kernel_size=3, stride=2, padding=1
-        )
-        self.n4 = C2f(c1=int(768 * w), c2=int(512 * w), n=round(3 * d), shortcut=False)
-        self.n5 = Conv_Block(
-            c1=int(512 * w), c2=int(512 * w), kernel_size=3, stride=2, padding=1
-        )
-        self.n6 = C2f(
-            c1=int(512 * w * (1 + r)),
-            c2=int(512 * w * r),
-            n=round(3 * d),
-            shortcut=False,
-        )
-
-    def return_modules(self):
-        return [self.n1, self.n2, self.n3, self.n4, self.n5, self.n6]
-
-    def __call__(self, p3, p4, p5):
-        x = self.n1(self.up(p5).cat(p4, dim=1))
-        head_1 = self.n2(self.up(x).cat(p3, dim=1))
-        head_2 = self.n4(self.n3(head_1).cat(x, dim=1))
-        head_3 = self.n6(self.n5(head_2).cat(p5, dim=1))
-        return [head_1, head_2, head_3]
